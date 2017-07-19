aa5dbf3d7427e922507f6a9af68667ca36238790
Filippo Boiani <filippo.boiani2@gmail.com>
2017-07-19 15:51:09 +0200

    SR up

--- src/main/resources/SocialRecord.sol
+++ src/main/resources/SocialRecord.sol
@@ -12,23 +12,23 @@ pragma solidity ^0.4.0;
 
 // SocialRecord contract must be mortal and owned
 contract SocialRecord {
-    
+
     struct User {
         string globalId;
         bool exists;
     }
-    
+
     // struct containing the Social Record
     struct SR {
-        string socialRecord; 
-        bool exists; 
+        string socialRecord;
+        bool exists;
     }
 
     address owner;
     mapping (address => User) users;
     // State variable
-    mapping (string => SR) srs; // maps GIDs to SocialRecords 
-    
+    mapping (string => SR) srs; // maps GIDs to SocialRecords
+
     // Only the owner can call the function
     modifier onlyOwner {
         require(msg.sender == owner);
@@ -47,7 +47,7 @@ contract SocialRecord {
             _;
         }
     }
-    
+
     // Constructor
     function SocialRecord(){
         owner = msg.sender;
@@ -56,74 +56,66 @@ contract SocialRecord {
     }
 
     // Events
-    event SocialRecordAdded(address user, string globalId);
-    event SocialRecordUpdated(address updater, string globalId);
+    event SocialRecordAdded(address user, string globalId, string socialRecordString);
+    event SocialRecordUpdated(address updater, string globalId, string socialRecordString);
     event SocialReocordDeleted(address deleter, string globalId);
 
-    // add a Social Record: only a non-user can create it and the globalId has to be new. 
+    // receives an hash and the signature parameters. With these it gets the signer address.
+    function verify(bytes32 _hash, uint8 _v, bytes32 _r, bytes32 _s) constant returns(address signerAddr) {
+        signerAddr = ecrecover(_hash, _v, _r, _s);
+    }
+
+    // add a Social Record: only a non-user can create it and the globalId has to be new.
     function addSocialRecord(string _globalId, string _socialRecordString) returns (bool, string) {
-        
+
         if (srs[_globalId].exists) {
-            return (false, ""); 
+            return (false, "");
         }
 
         srs[_globalId] = SR(_socialRecordString, true);
         users[msg.sender] = User(_globalId, true);
-        SocialRecordAdded(msg.sender, _globalId);
-        
+        SocialRecordAdded(msg.sender, _globalId, _socialRecordString);
+
         return(true, _socialRecordString);
     }
 
     // get a Social Record
     function getSocialRecord(string _globalId) constant returns (string) {
-        
+
         if (!srs[_globalId].exists) {
-           throw; 
+           throw;
         }
 
         return srs[_globalId].socialRecord;
     }
 
-    function verify( string memory _hash, uint8 _v, string memory _r, string memory _s) constant returns(address retAddr) {
-        bytes32 hash;
-        bytes32 r;
-        bytes32 s;
-        assembly {
-            hash := mload(add(_hash, 32))
-            r := mload(add(_r, 32))
-            s := mload(add(_s, 32))
-        }
-
-        retAddr = ecrecover(hash, _v, r, s);
-    }
-
-    // update a Social Record 
-    // TODO: import library to compare strings 
+    // update a Social Record
+    // TODO: import library to compare strings
     function updateSocialRecord(string _globalId, string _socialRecordString) returns (bool, string){
-        
-        // the user is the owner of the social record 
-        // if (sha3(users[msg.sender].globalId) != sha3(_globalId)) {
-        //     return false; 
-        // }
+
+        // the user is the owner of the social record
+        if (sha3(users[msg.sender].globalId) != sha3(_globalId)) {
+            return (false, "This account is not allowed to modify the social record corresponding to the specified global id");
+        }
 
         // the Social Record must exist
         if (!srs[_globalId].exists)  {
-            return (false, ""); 
+            return (false, "The specified social record doesn't exist");
         }
 
         srs[_globalId] = SR(_socialRecordString, true);
         // Trigger the event
-        SocialRecordUpdated(msg.sender, _globalId);
-        return (true, _socialRecordString); 
+        SocialRecordUpdated(msg.sender, _globalId, _socialRecordString);
+        return (true, _socialRecordString);
     }
 
     // delete a Social Record
     function deleteSocialRecord(string _globalId) returns (bool) {
         delete srs[_globalId];
         delete users[msg.sender];
-        return true; 
+        return true;
     }
-    
+
     // Kill the contract (only the owner can)
     function kill() onlyOwner {
         selfdestruct(owner);
